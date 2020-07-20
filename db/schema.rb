4cc45ff7ea8a4749519188f4c37fd0a6a7e94eae
LoboArkano <pipiolo_mananes@hotmail.com>
2020-07-20 17:30:10 -0500

    Fix rubocop errors

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,14 +12,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_162513) do
-
-  create_table "cars", force: :cascade do |t|
-    t.string "make"
-    t.string "model"
-    t.integer "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_200_213_162_513) do
+  create_table 'cars', force: :cascade do |t|
+    t.string 'make'
+    t.string 'model'
+    t.integer 'year'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
